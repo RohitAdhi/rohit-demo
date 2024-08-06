8921763f7e3b3834f61b73cfa0532c95c303d18b
RohitAdhi <rohitadhikari013@gmail.com>
2024-08-06 10:40:44 +0530

    PrintFiftyNumber
    using While Loop

---
+++ LocalRepo/Projects/Practices/whileLoop.java
@@ -0,0 +1,13 @@
+package LocalRepo.Projects.Practices;
+
+public class whileLoop {
+    public static void main(String[] args) {
+        int num = 1;
+        while (num < 50) {
+            num += 1;
+            System.out.println(num);
+
+        }
+
+    }
+}
