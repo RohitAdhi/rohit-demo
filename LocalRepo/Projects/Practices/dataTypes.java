c2c70e87413c03fee04f72b160372437a85af2a1
RohitAdhi <rohitadhikari013@gmail.com>
2024-08-06 10:04:31 +0530

    sum of  ThreeNumber
    Add

---
+++ LocalRepo/Projects/Practices/dataTypes.java
@@ -0,0 +1,11 @@
+package LocalRepo.Projects.Practices;
+
+public class dataTypes {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        System.out.println(a + b + c);
+    }
+
+}
